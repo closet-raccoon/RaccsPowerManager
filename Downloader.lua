37e978d9da9551e2ba68c7fdf0763e599ce34386
Closet Raccoon <p.keagan@outlook.com>
2021-03-31 15:30:36 -0600

    Update Downloader.lua

--- Downloader.lua
+++ Downloader.lua
@@ -1,70 +1,112 @@
 -- Installer for RPM
 -- Feel free to edit, redistrabute and copy!
+
 local args = ...
-local startup
 
-links = {
-    RPM = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/RPM.lua",
-    API = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/ClosetAPI.lua",
-    startup = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/startup.lua",
-}
+local use_live_file_list = true
+local link_location = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/filelist"
+local links, startup
 
-if args[1] == "help" then
-    print("Download RPM files and APIs")
-    print("Argument 1 is a boolean ( True of False ),")
-    print(" on whether or not it should install it with a startup.lua")
-    print("By default it installs as startup!")
-end
 
-if args[1] == "" or args[1] == nil or args[1] == "true" then
-    print("Installing has startup!")
+if type(args) == "table" then
+    if args[1] == "help" then
+        print("Download RPM files and APIs")
+        print("Argument 1 is a boolean ( True of False ),")
+        print(" on whether or not it should install it with a startup.lua")
+        print("By default it installs as startup!")
+    end
+
+    if args[1] == "" or args[1] == nil or args[1] == "true" then
+        print("Installing has startup!")
+        startup = true
+    elseif args[1] == false then
+        print("Installing without startup!")
+        startup = false
+    end
+else
     startup = true
-elseif args[1] == false then
-    print("Installing without startup!")
-    startup = false
 end
+
+local color = term.isColor() == true
+
+function printlink(link)
+    local oldc = term.getTextColor()
+    if color == true then
+        term.setTextColor(colors.lightBlue)
+    end
+    print(link)
+    term.setTextColor(oldc)
+end
+
+function getLinks()
+    if use_live_file_list == false or type(link_location) ~= string then
+        print("Using hard coded links!")
+        return {
+            RPM = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/RPM.lua",
+            ClosetAPI = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/ClosetAPI.lua",
+            startup = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/startup.lua",
+        }
+    else
+        local resp = http.get(link_location)
+        if type(resp) == "table" then
+            local data = resp.readAll()
+            resp.close()
+            local finished = textutils.unserialize(data)
+            if finished == nil or finished ~= table then
+                print("Was not able to download links from "..link_location)
+                print("Will continue with hard coded links!")
+                return {
+                    RPM = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/RPM.lua",
+                    ClosetAPI = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/ClosetAPI.lua",
+                    startup = "https://raw.githubusercontent.com/closet-raccoon/RaccsPowerManager/main/startup.lua",
+                }
+            else
+                print("Found links from"..link_location)
+                return finished
+            end
+        end
+    end
+end
+
+links = getLinks()
 
 function DownloadAll(install_startup_module)
+    local files = {}
     if not install_startup_module then
         install_startup_module = true
     end
 
-    local RPM_File, ClosetAPI_File, startup_File
-    local resp = http.get(links.RPM)
-    if resp then
-        RPM_File = resp.readAll()
-        resp.close()
-    end
-    local resp = http.get(links.API)
-    if resp then
-        ClosetAPI_File = resp.readAll()
-        resp.close()
-    end
-    if install_startup_module == true then
-        resp = http.get(links.startup)
-        if resp then
-            startup_File = resp.readAll()
-            resp.close()
+    for k,v in pairs(links) do
+        if ((k == "startup" and install_startup_module == true) or k ~= "startup") and  type(k) == "string"   then
+            print("Downloading "..k.." from ")
+            printlink("'"..v.."'")
+            local data
+            local resp = http.get(v)
+            if resp then data = resp.readAll() end
+            files[k] = data
+        else
+           term.write("Could not download data for '"..k.."' with link")
+           printlink("'"..v.."'")
         end
     end
-    return {
-        ['RPM'] = RPM_File,
-        ['ClosetAPI'] = ClosetAPI_File,
-        ['startup'] = startup_File,
-    }
+    return files
 end
 
 function SaveDownloads(downloaded_files)
+    local ver_high = tonumber( string.sub( os.version(), 8 ) ) >= 1.8
+    if ver_high == true then print("You are on version 1.8 or higher of ComputerCraft, using .lua files") end
+
     for k,v in pairs(downloaded_files) do
         if v and type(v) == "string" then
-            if tonumber( string.sub( os.version() ,8) ) >= 1.8 then
+            if ver_high == true then  --Version check
                 k = k..".lua"
             end
-            local file = fs.open(k,"w")
+            local file = fs.open(k,"w") --Opening and saving to file
             if type(file) == "table" then
                 file.write(v)
                 file.flush()
                 file.close()
+                print("Saved to "..k)
             else
                 print("Failed to save file "..k)
                 print("File returned "..file)
@@ -76,6 +118,7 @@ end
 
 if http then
     if fs then
+        print("Downloading RPM!")
         SaveDownloads(DownloadAll(startup))
     else
         error("fs API not found, is the module loaded?")
@@ -83,3 +126,4 @@ if http then
 else
     error("http API not found, is the module loaded?")
 end
+print("Completed thanks for using Raccs Installer!")
